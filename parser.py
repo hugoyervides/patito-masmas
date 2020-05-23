ab4301db82912f0e091ee4e987cd20f5c593e94f
Victor Oyervides <victor.covarrubias@hotmail.com>
2020-05-22 21:36:20 -0500

    final structure changes

--- parser.py
+++ parser.py
@@ -6,12 +6,14 @@ from handlers.fun_handler import Funhandler
 from data_structures.constant_table import Constanttable
 import sys
 
+
 #Variable declaration
 stacks = Stacks()
 var_tables = Vartables()
 fun_handler = Funhandler()
 constant_table = Constanttable()
 for_stack = []
+
 
 start = 'PROG'
 
@@ -255,6 +257,7 @@ def error_handler(line, error):
     print("Error in line", line, error)
     sys.exit()
 
+
 def p_error(p):
     if p:
         print("Syntax error at line: ",p.lineno , p.type)
@@ -271,7 +274,6 @@ def p_error(p):
 
 def p_r_new_id(p):
     'r_new_id : '
-
     type_var = var_tables.get_var_type(p[-1])
     mem_address = var_tables.get_virtual_mem(p[-1])
     stacks.register_operand(mem_address)
@@ -573,70 +575,6 @@ def p_r_new_write(p):
     'r_new_write : '
     stacks.generate_write_quadruple()
 
+#Construct the parser
 parser = yacc.yacc()
-
-testScript = '''
-    programa patito; 
-    var
-    int id1, id2, id3, a, b, c, i;
-    float flo;
-    char letra;
-    funcion void prueba(int y, float x)
-    var 
-        int i, a, b, j;
-    {
-        i = j + i;
-        si ( a > b ) entonces {
-            a = a+1;
-            b = (10 + 15) * 7;
-            
-        } sino {
-            b = 1 +1;
-        }
-    }
-    funcion int patito(int x1, int f, char s)
-    var
-        float x;
-        int y;
-        char e;
-    {
-        x = 1 + 1;
-        y = patito(1,2,'s');
-        return(y + 12 * 5);
-        e = 'E' ;
-        e = 'S';
-        
-    }
-    principal(){
-        id1 = patito(3 , 5 * id2 + id1 , 'e') * 5;
-        si ( id1 >= id2) entonces {
-            a = a+1;
-            b = (10 + 15) * 7;
-        } sino {
-            b = 1 +1;
-        }
-        a =  b = 1 + id2 * (10 * (id1 + 55)) * id3;
-        mientras (a > b) haz{
-            a = a + 1;
-        }
-        mientras ( c > a) haz{
-            a = 2 + 1;
-        }
-        a = 2;
-        desde i = 0 hasta 9 hacer{
-            a = 10;
-        }
-
-        a = i + 1;
-
-        a = 12;
-        lee(id1);
-        escribe(id2, 3, 2, id1);
-    }
-'''
-
-parser.parse(testScript)
-#printTable()
-stacks.quadruples.display_quadruples()
-fun_handler.funcTable.display_fun_table()
 
